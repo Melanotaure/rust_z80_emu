420975bc063a6fd6b075037291f1aadbdeed5ef4
unknown <luc.gaborit@gmail.com>
2023-11-26 15:39:56 +0100

    create a module for register

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,6 @@
-union Register {
-    reg16: u16,
-    reg8: [u8; 2],
-}
+mod register;
 
-impl Register {
-    fn new() -> Self {
-        Self { reg16: 0 }
-    }
-
-    fn get_reg16(&self) -> u16 {
-        unsafe { self.reg16 }
-    }
-
-    fn get_reg8_h(&self) -> u8 {
-        unsafe { self.reg8[1] }
-    }
-
-    fn get_reg8_l(&self) -> u8 {
-        unsafe { self.reg8[0] }
-    }
-
-    fn set_reg16(&mut self, value: u16) {
-        self.reg16 = value;
-    }
-
-    fn set_reg8_h(&mut self, value: u8) {
-        unsafe {
-            self.reg8[1] = value;
-        }
-    }
-
-    fn set_reg8_l(&mut self, value: u8) {
-        unsafe {
-            self.reg8[0] = value;
-        }
-    }
-
-    fn add_r16_r16(&mut self, rh: &Register) {
-        self.set_reg16(self.get_reg16() + rh.get_reg16());
-    }
-
-    fn add_r16_i8(&mut self, rh: u8) {
-        self.set_reg16(self.get_reg16() + rh as u16);
-    }
-}
+use register::*;
 
 struct AmsRegs {
     af: Register,
---
+++ src/register.rs
@@ -0,0 +1,46 @@
+pub union Register {
+    reg16: u16,
+    reg8: [u8; 2],
+}
+
+impl Register {
+    pub fn new() -> Self {
+        Self { reg16: 0 }
+    }
+
+    pub fn get_reg16(&self) -> u16 {
+        unsafe { self.reg16 }
+    }
+
+    pub fn get_reg8_h(&self) -> u8 {
+        unsafe { self.reg8[1] }
+    }
+
+    pub fn get_reg8_l(&self) -> u8 {
+        unsafe { self.reg8[0] }
+    }
+
+    pub fn set_reg16(&mut self, value: u16) {
+        self.reg16 = value;
+    }
+
+    pub fn set_reg8_h(&mut self, value: u8) {
+        unsafe {
+            self.reg8[1] = value;
+        }
+    }
+
+    pub fn set_reg8_l(&mut self, value: u8) {
+        unsafe {
+            self.reg8[0] = value;
+        }
+    }
+
+    pub fn add_r16_r16(&mut self, rh: &Register) {
+        self.set_reg16(self.get_reg16() + rh.get_reg16());
+    }
+
+    pub fn add_r16_i8(&mut self, rh: u8) {
+        self.set_reg16(self.get_reg16() + rh as u16);
+    }
+}
