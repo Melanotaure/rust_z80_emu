c85595d51fc6d1430bc799184a83375e7dd40573
unknown <luc.gaborit@gmail.com>
2024-01-07 17:09:06 +0100

    snake_case refacto

--- src/z80.rs
+++ src/z80.rs
@@ -10,21 +10,21 @@ pub struct Z80 {
     // Data bus
     pub dbus: u8,
     // System control pins
-    pub nM1: bool,
-    pub nMREQ: bool,
-    pub nIORQ: bool,
-    pub nRD: bool,
-    pub nWR: bool,
-    pub nRFSH: bool,
+    pub n_m1: bool,
+    pub n_mreq: bool,
+    pub n_iorq: bool,
+    pub n_rd: bool,
+    pub n_wr: bool,
+    pub n_rfsh: bool,
     // CPU control
-    pub nHALT: bool,
-    pub nWAIT: bool,
-    pub nINT: bool,
-    pub nNMI: bool,
-    pub nRESET: bool,
+    pub n_halt: bool,
+    pub n_wait: bool,
+    pub n_int: bool,
+    pub n_nmi: bool,
+    pub n_reset: bool,
     // CPU bus control
-    pub nBUSRQ: bool,
-    pub nBUSACK: bool,
+    pub n_busrq: bool,
+    pub n_busack: bool,
     // Added the clock pin but might not be used
     pub _clock: bool,
 }
@@ -35,19 +35,19 @@ impl Z80 {
             regs: Z80Registers::new(),
             abus: 0_u16,
             dbus: 0_u8,
-            nM1: true,
-            nMREQ: true,
-            nIORQ: true,
-            nRD: true,
-            nWR: true,
-            nRFSH: true,
-            nHALT: true,
-            nWAIT: true,
-            nINT: true,
-            nNMI: true,
-            nRESET: true,
-            nBUSRQ: true,
-            nBUSACK: true,
+            n_m1: true,
+            n_mreq: true,
+            n_iorq: true,
+            n_rd: true,
+            n_wr: true,
+            n_rfsh: true,
+            n_halt: true,
+            n_wait: true,
+            n_int: true,
+            n_nmi: true,
+            n_reset: true,
+            n_busrq: true,
+            n_busack: true,
             _clock: true,
         }
     }
@@ -56,19 +56,19 @@ impl Z80 {
         self.regs.reset();
         self.abus = 0_u16;
         self.dbus = 0_u8;
-        self.nBUSACK = true;
-        self.nBUSRQ = true;
-        self.nHALT = true;
-        self.nINT = true;
-        self.nIORQ = true;
-        self.nM1 = true;
-        self.nMREQ = true;
-        self.nNMI = true;
-        self.nRD = true;
-        self.nRESET = true;
-        self.nRFSH = true;
-        self.nWAIT = true;
-        self.nWR = true;
+        self.n_busack = true;
+        self.n_busrq = true;
+        self.n_halt = true;
+        self.n_int = true;
+        self.n_iorq = true;
+        self.n_m1 = true;
+        self.n_mreq = true;
+        self.n_nmi = true;
+        self.n_rd = true;
+        self.n_reset = true;
+        self.n_rfsh = true;
+        self.n_wait = true;
+        self.n_wr = true;
         self._clock = true;
     }
 }
