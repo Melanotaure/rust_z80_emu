591988436399609ca5b870e43b2a3e775f7095c9
unknown <luc.gaborit@gmail.com>
2026-05-15 12:41:28 +0200

    add zexdoc tests

---
+++ examples/zexdoc_test.rs
@@ -0,0 +1,32 @@
+use rust_z80_emu::z80::*;
+// use std::io;
+
+fn main() {
+    let mut z80 = Z80::new();
+
+    let code = std::fs::read("resources/zexdoc.com").unwrap();
+    for (addr, opcode) in code.iter().enumerate() {
+        if addr >= 0x13 {
+            z80.bus.write((addr + 0x100) as u16, *opcode);
+        } else {
+            z80.bus.write(addr as u16, *opcode);
+        }
+    }
+
+    let mut cycles: usize = 0;
+
+    loop {
+        cycles += z80.execute() as usize;
+        if z80.n_halt == false {
+            break;
+        }
+
+        // z80.memory_dump(0x0000, 0x22ff);
+        // println!("");
+        // z80.display_regs();
+
+        // let mut input = String::new();
+        // io::stdin().read_line(&mut input).unwrap();
+    }
+    println!("cycles: {}", cycles);
+}
